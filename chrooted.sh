49f460d44d82a23b6d707a8c68c8b629d7886835
Kaden Cartwright <cartwrightkaden@gmail.com>
2025-04-10 14:08:18 -0500

    reorganize install scripts

--- chrooted.sh
+++ chrooted.sh
@@ -1,23 +1,35 @@
 #!/bin/env sh
 echo "Setting Timezone"
 ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime
+echo "installing packages for graphical environment"
+pacman -S --noconfirm - < /root/packages/wm.txt
 
 hwclock --systohc
 sed -i -e 's|#en_US.UTF-8 UTF-8|en_US.UTF-8 UTF-8|' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >/etc/locale.conf
 
+echo "setting password for root user"
 echo -n "$PASSWORD" | passwd --stdin
+echo "creating user: 'k'"
 USERNAME=k
 useradd $USERNAME -m 
+
+
+
+su - k -c /root/install-yay.sh 
+echo "setting password for user: 'k'"
 echo -n "$PASSWORD" | passwd $USERNAME --stdin
 usermod -aG wheel $USERNAME
 echo '%wheel ALL=(ALL:ALL) ALL' | sudo EDITOR='tee -a' visudo
 # set hostname
 echo $HOSTNAME > /etc/hostname
 
+echo "Running bootctl install"
 bootctl install
 
+echo "Running dracut"
 dracut --regenerate-all 
 
+echo "enabling NetworkManager service"
 systemctl enable NetworkManager
--- pacstrap.sh
+++ pacstrap.sh
@@ -5,8 +5,4 @@
 # gum
 echo "Running Pacstrap"
 pacstrap -K /mnt base linux base-devel linux-firmware lvm2 neovim git \
-    networkmanager amd-ucode man-db man-pages pipewire pipewire-jack \
-    pipewire-audio pipewire-alsa hyprland xdg-desktop-portal-hyprland \
-    zsh zsh-autosuggestions zsh-completions hyprpolkitagent power-profiles-daemon \
-    ttf-meslo-nerd ttf-dejavu-nerd pipewire-pulse network-manager-applet dracut \
-    bluez bluez-utils rpcbind
+    networkmanager amd-ucode man-db man-pages  dracut bluez bluez-utils rpcbind

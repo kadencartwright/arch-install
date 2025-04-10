5d986b19598fa259bee6ca181548e15776c6fb11
Kaden Cartwright <cartwrightkaden@gmail.com>
2025-04-10 10:07:42 -0500

    replace gnome-polkit with hyprpolkitagent

--- pacstrap.sh
+++ pacstrap.sh
@@ -7,6 +7,6 @@ echo "Running Pacstrap"
 pacstrap -K /mnt base linux base-devel linux-firmware lvm2 neovim git \
     networkmanager amd-ucode man-db man-pages pipewire pipewire-jack \
     pipewire-audio pipewire-alsa hyprland xdg-desktop-portal-hyprland \
-    zsh zsh-autosuggestions zsh-completions polkit-gnome power-profiles-daemon \
+    zsh zsh-autosuggestions zsh-completions hyprpolkitagent power-profiles-daemon \
     ttf-meslo-nerd ttf-dejavu-nerd pipewire-pulse network-manager-applet dracut \
     bluez bluez-utils rpcbind

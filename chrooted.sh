dd23d01355af47a5edda520c47037b4675025fd7
Kaden Cartwright <cartwrightkaden@gmail.com>
2025-11-26 16:18:35 -0600

    fixed uki gen

--- chrooted.sh
+++ chrooted.sh
@@ -51,8 +51,33 @@ echo -n "$LUKS_PASSPHRASE" > /tmp/luks-key
 systemd-cryptenroll --wipe-slot tpm2 --tpm2-device auto --unlock-key-file=/tmp/luks-key $LUKS_PARTITION
 rm /tmp/luks-key
 
+ln -s /dev/null /etc/pacman.d/hooks/90-dracut-install.hook
 echo "Running dracut"
 dracut --regenerate-all 
+
+# 1. Identify the root device currently mounted
+ROOT_SOURCE=$(findmnt / -n -o SOURCE)
+ROOT_FSTYPE=$(findmnt / -n -o FSTYPE)
+
+# 2. Get LVM details
+eval $(lvs --noheadings --nameprefixes -o vg_name,lv_name "$ROOT_SOURCE")
+LVM_ARG="rd.lvm.lv=${LVM2_VG_NAME}/${LVM2_LV_NAME}"
+
+# 3. Get LUKS details 
+LUKS_UUID=$(lsblk -s -p -n -o UUID,FSTYPE "$ROOT_SOURCE" | grep crypto_LUKS | awk '{print $1}')
+
+if [ -n "$LUKS_UUID" ]; then
+    LUKS_ARG="rd.luks.uuid=luks-${LUKS_UUID}"
+fi
+
+CMDLINE="${LUKS_ARG} ${LVM_ARG} root=/dev/mapper/${LVM2_VG_NAME}-${LVM2_LV_NAME} rootfstype=${ROOT_FSTYPE} rootflags=rw,relatime"
+
+# 5. Write to file
+mkdir -p /etc/kernel
+echo "$CMDLINE" > /etc/kernel/cmdline
+
+echo "Generated Configuration:"
+echo "$CMDLINE"
 
 echo "enabling NetworkManager service"
 systemctl enable NetworkManager
--- install-hooks.sh
+++ install-hooks.sh
@@ -3,13 +3,6 @@
 mkdir -p /etc/pacman.d/hooks
 cp /root/cfgs/95-systemd-boot.hook /etc/pacman.d/hooks/95-systemd-boot.hook
 
-cp /root/cfgs/dracut-install.sh /usr/local/bin/dracut-install.sh
-
-cp /root/cfgs/dracut-remove.sh /usr/local/bin/dracut-remove.sh
-
-cp /root/cfgs/90-dracut-install.hook /etc/pacman.d/hooks/90-dracut-install.hook
-
-cp /root/cfgs/60-dracut-remove.hook /etc/pacman.d/hooks/60-dracut-remove.hook
 
 cp /root/cfgs/hostonly.conf /etc/dracut.conf.d/hostonly.conf
 cp /root/cfgs/uefi.conf /etc/dracut.conf.d/uefi.conf
@@ -19,5 +12,3 @@ grep "ILoveCandy" /etc/pacman.conf >/dev/null || sed -i "/#VerbosePkgLists/a ILo
 grep "^ParallelDownloads" /etc/pacman.conf >/dev/null || sed -i "s/^#ParallelDownloads/ParallelDownloads/" /etc/pacman.conf
 sed -i "s/-j2/-j$(nproc)/;s/^#MAKEFLAGS/MAKEFLAGS/" /etc/makepkg.conf
 
-chmod +x /usr/local/bin/dracut-install.sh
-chmod +x /usr/local/bin/dracut-remove.sh
